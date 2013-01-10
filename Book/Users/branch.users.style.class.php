28bd4b05dfd5da8fec0d6f9fb85649e6325b777f
Cikica <aleksandar.winter@gmail.com>
2013-01-10 12:15:59 +0000

    Users it looking fly, but were moving on son, right nows

--- Book/Users/branch.users.style.class.php
+++ Book/Users/branch.users.style.class.php
@@ -53,8 +53,7 @@ class branch_users_style extends alpha_tree_users
 
 					<?php $this->profile_managment($config['create_table']['name']); ?>
 
-					<input data-function-to-call="add_new_field_input" type="button" value="Add Field">
-					<input type="button" value="Remove Field">
+					<input data-function-to-call="add_new_field_input" class="profile_button" type="button" value="Add Field">
 
 				</div>
 
@@ -75,6 +74,7 @@ class branch_users_style extends alpha_tree_users
 					lf_users.init(".<?php echo $this->params['id']; ?>-body");
 					lf_users.create_global_variable("loader_path", "<?php echo AJAXLOADS; ?>");
 					lf_users.create_global_variable("user_options", "<?php echo $config['options']?>");
+					lf_users.create_global_variable("user_field_box_class", ".users_profile_box");
 					console.log(lf_users.global);
 
 				</script>
@@ -98,157 +98,18 @@ class branch_users_style extends alpha_tree_users
  	}
 
  	protected function _field_option_box ($key, $field)
- 	{ ?>
+ 	{	
+		$template_options = 
+			array(
+				'template_options' => 
+					array(
+						'name' => 'user',
+						'user_options' => $this->params['manifestation']['options'],
+						'field' => $field
+			));	
 
- 		<?php 
- 			$user_options = $this->params['manifestation']['options'];
- 			$data_types_for_conversion_into_options = 
-				array(
-					array( 
-						'name' => 'Post Code', 										
-						'value' => 'post_code' ),
-					array( 
-						'name' => 'Small Text', 				
-						'value' => 'smalltext' ),
-					array( 
-						'name' => 'Moderate Text', 			
-						'value' => 'medium_text' ),
-					array( 
-						'name' => 'Alot of text', 			
-						'value' => 'alot_of_text' ),
-					array( 
-						'name' => 'The Year', 				
-						'value' => 'just_year' ),
-					array( 
-						'name' => 'Full Date', 				
-						'value' => 'the_date' ),
-					array( 
-						'name' => 'Hours and Minutes', 		
-						'value' => 'the_time' ),
-					array( 
-						'name' => 'A Url',	 				
-						'value' => 'url' ),
-					array( 
-						'name' => 'The Email', 				
-						'value' => 'email' ),
-					array( 
-						'name' => 'Money Input', 			
-						'value' => 'money' ),
-					array( 
-						'name' => 'Decimal Number',          
-						'value' => 'decimal' ),
-					array( 
-						'name' => 'Small Number(1-100)',     
-						'value' => 'small_number' ),
-					array( 
-						'name' => 'Medium Number(1-100000)', 
-						'value' => 'medium_number' ),
-					array( 
-						'name' => 'Huge Number(100000+)',    
-						'value' => 'huge_number' ));
-		?> 
-
-		<div class="users_profile_box">
-
-			<!-- Field name -->
-			<div class="user_profile_box_field">
-
-				<div class="user_profile_desc">
-					<strong>Field Name</strong>
-					<span>Desc</span>
-				</div>
-				
-				<div class="user_profile_input">
-					<input type="text" name="<?php echo $user_options ."[$key]"; ?>[field_name]" value="<?php echo $field['field_name']; ?>">
-				</div>
-			</div>
-
-			<!-- Description -->
-			<div class="user_profile_box_field">
-					
-				<div class="user_profile_desc">
-					<strong>Description</strong>
-					<span>Desc</span>
-				</div>
-
-				<div class="user_profile_input">
-					<textarea name="<?php echo $user_options ."[$key]"; ?>[description]"><?php echo $field['description']; ?></textarea>
-				</div>
-			</div>
-
-			<!-- Help them along -->
-			<div class="user_profile_box_field">
-					
-				<div class="user_profile_desc">
-					<strong>Incorrect Input</strong>
-					<span>Desc</span>
-				</div>
-
-				<div class="user_profile_input">
-					<textarea name="<?php echo $user_options ."[$key]"; ?>[help_them_along]"><?php echo $field['help_them_along']; ?></textarea>
-				</div>
-			</div>
-
-			<!-- Input Type -->
-			<div class="user_profile_box_field">
-					
-				<div class="user_profile_desc">
-					<strong>Input Type</strong>
-					<span>Desc</span>
-				</div>
-
-				<div class="user_profile_input">
-					<select name="<?php echo $user_options ."[$key]"; ?>[field_input_type]" id="">				
-						<?php option_spitter( $data_types_for_conversion_into_options, $field['field_input_type'] ); ?>
-					</select>
-				</div>
-			</div>
-
-			<!-- Unique Field -->
-			<div class="user_profile_box_field">
-
-				<div class="user_profile_desc">
-					<strong>Make Unique</strong>
-					<span>Desc</span>
-				</div>
-
-				<div class="user_profile_input">						
-					<select name="<?php echo $user_options ."[$key]"; ?>[is_unique]">
-
-						<option value="1" <?php selected( $field['is_unique'], '1'); ?>>Unique</option>
-
-						<option value="0" <?php selected( $field['is_unique'], '0'); ?>>Not Unique</option>
-
-					</select>
-				</div>
-			</div>
-
-			<!-- Required Field -->
-			<div class="user_profile_box_field">
-
-				<div class="user_profile_desc">
-					<strong>Make Required</strong>
-					<span>Desc</span>
-				</div>
-			
-				<div class="user_profile_input">
-
-					<select name="<?php echo $user_options ."[$key]"; ?>[required]">
-
-						<option value="1" <?php selected( $field['required'], '1'); ?>>Required</option>
-
-						<option value="0" <?php selected( $field['required'], '0'); ?>>Not Required</option>
-
-					</select>
-				</div>
-			</div>
-
-			<input data-function-to-call="" type="button" value="Remove Field" class="profile_button">
-			
-		</div>
-<?php }
-
-
+			require ( FRAMEWORK ."/ajax_loads/load/user.load.php");
+	}
 }
 
 ?>
--- Framework/scripts/adder.users.js
+++ Framework/scripts/adder.users.js
@@ -2,18 +2,27 @@ var lf_users = (function ($, lf_users) {
 
 	lf_users.add_new_field_input = function (current_click, event) { 
 
+		current_click.element.val("Loading...")
+
 		$.ajax({
 			type : 'GET',
 			url : this.global.loader_path,
 			data : { template_options : { name : "user", user_options : this.global.user_options } },
 			dataType : 'html',
-			sucess : function (response) { 
-
+			success : function (response) { 
+				// $(lf_users.global.user_field_box_class).last().after(response);
+				// current_click.element.before(response);	
+				$(response).css('opacity', '0').insertBefore(current_click.element).animate({ opacity : '1'}, 400);
+				current_click.element.val("Add Field");
 			}
 		});
-		console.log(this.global.loader_path);
-		console.log(this.global.user_options);
+
 	};
+
+	lf_users.remove_field = function (current_click, event) { 
+
+		current_click.element.parent().fadeOut(400, function () { $(this).empty().remove();});
+	}
 
 	return lf_users;
 
