b3dca0b9bd0188cbf8edf15e8839a1b228bf76f5
Cikica <aleksandar.winter@gmail.com>
2013-01-30 17:27:39 +0000

    Ticketing adding to basket works

--- Framework/scripts/ticket.alpha.js
+++ Framework/scripts/ticket.alpha.js
@@ -30,13 +30,15 @@ var alpha = (function ( alpha, $ ) {
 
 	alpha.search_though_amazon_for_a_book = function (current_click) { 
 		
-		var instructions = current_click.instructions;
+		var instructions = current_click.instructions,
 			typed_value  = $(instructions.input).val().trim(),
 			search_by    = (alpha._is_number(typed_value)? instructions.numerical_search : 'keywords' ),
 			klass        = this.prototype;
 		
 		current_click.element.text('Searching...');
-		console.log(typed_value);
+
+		klass.basket('.ticket_basket');
+
 		$.post(
 			ajaxurl,
 			{ action : instructions.action, paramaters : { typed : typed_value, search_by : search_by, search_for : instructions.search_for, filter_name : instructions.filter_by }},
@@ -52,50 +54,63 @@ var alpha = (function ( alpha, $ ) {
 		var books_to_display = alpha.amazon.prototype.return_books(books),
 			books            = this.endowment(books_to_display),
 			book_wrap        = $(book_wrap);
-			
+	
 			book_wrap.fadeOut(400,
 			function () {
 				
 				$(this).empty();
 
-				$.each(books_to_display, 
-				function (index, book) {
-					book.manifest(book_wrap);
-				});
+					$.each(books_to_display, 
+					function (index, book) {
+						book.manifest(book_wrap);
+						// book.insert_into_basket();
+					});
 
 				$(this).fadeIn(400);
 			});
 	};	
 
 	alpha.search_though_amazon_for_a_book.prototype.endowment = function (to_endow) { 
-		
+
+		var prototype = this;
+
 		$.each(to_endow,
 		function (index, endow) { 
 
 			endow.insert_into_basket = function () { 
 
-				
+				prototype.basket.prototype.update(this);
 
 			};
 
 			endow.manifest = function (element_to_append_to) { 
 
 				var quote  = '21',
+					klass  = this,
 					string =
-							'<div class="ticket_book_wrap">'+
-							'<div class="ticket_book_thumbnail">'+
-								'<img src="'+ this.image +'">'+
-							'</div>'+
-							'<div class="ticket_book_details">'+
-								'<span class="ticket_book_detail">Title: <strong>'+  this.title  +'</strong></span>'+
-								'<span class="ticket_book_detail">Author: <strong>'+ this.author +'</strong></span>'+
-								'<span class="ticket_book_detail">ISBN: <strong>'+   this.isbn   +'</strong></span>'+
-								'<div class="ticket_quoted_ammount"><span class="we_quote">We Quote</span><span class="quote">'+ quote +'</span></div>'+
-								'<div class="ticket_button">Add Book</div>'+
-							'</div>'+
-							'</div>';
+						'<div class="ticket_book_wrap">'+
+						'<div class="ticket_book_thumbnail">'+
+							'<img src="'+ this.image +'">'+
+						'</div>'+
+						'<div class="ticket_book_details">'+
+							'<span class="ticket_book_detail">Title: <strong>'+  this.title  +'</strong></span>'+
+							'<span class="ticket_book_detail">Author: <strong>'+ this.author +'</strong></span>'+
+							'<span class="ticket_book_detail">ISBN: <strong>'+   this.isbn   +'</strong></span>'+
+							'<div class="ticket_quoted_ammount"><span class="we_quote">We Quote</span><span class="quote">'+ quote +'</span></div>'+
+						'</div>'+
+						'</div>';
 
-					element_to_append_to.prepend(string);
+					var button = $('<div class="ticket_button">Add Book</div>').on('click', 
+					function () { 
+						klass.insert_into_basket(); 
+					});
+
+					$(string)
+					.children('.ticket_book_details')
+					.append(button)
+					.parent()
+					.prependTo(element_to_append_to);
+					// element_to_append_to.prepend(string);
 			};
 		});
 
@@ -104,9 +119,30 @@ var alpha = (function ( alpha, $ ) {
 	};
 
 	alpha.search_though_amazon_for_a_book.prototype.quote = function () { 
+	};
 
+	alpha.search_though_amazon_for_a_book.prototype.basket = function (element_to_be_basket) { 
 
-	}
+		this.basket.prototype.element = $(element_to_be_basket);
+		this.basket.prototype.store   = new Object;		
+		this.basket.prototype.update  = function (value_to_insert) { 
+
+			this.store[value_to_insert.isbn] = value_to_insert;
+			this.manifest(value_to_insert);
+			
+		};
+		this.basket.prototype.manifest = function (value_to_insert) { 
+
+			this.element.append(
+				'<div class="ticket_basket_book">'+
+					'<span class="ticket_basket_book_name">Title: <strong>'+ value_to_insert.title +'</strong></span>'+
+					'<span class="ticket_basket_book_name">ISBN: <strong>'+  value_to_insert.isbn  +'</strong></span>'+
+					'<span class="ticket_basket_book_name">£: <strong>'+     value_to_insert.price +'</strong></span>'+
+				'</div>'
+			);
+		};
+
+	};
 
 	return alpha;
 
