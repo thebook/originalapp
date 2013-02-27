ed4b0f4cdbbd921d8ca2b29f8ddf9e35ec3f6df9
Cikica <aleksandar.winter@gmail.com>
2013-02-27 13:49:33 +0000

    Style is all good

--- home.php
+++ home.php
@@ -9,9 +9,14 @@
 		<div class="logo_for_bar"></div>
 
 		<!-- Progress bar  -->
-		<div class="progress_icon_for_bar">			
+		<!-- <div class="progress_icon_for_bar">						
 			<span class="progress_icon_for_bar_text">Confirm</span>
-		</div>
+		</div> -->
+
+		<!-- Navigation  -->
+		<div class="navigation_text_for_bar">How It Works</div>
+		<div class="navigation_text_for_bar">Recyclabus</div>
+		<div class="navigation_text_for_bar">Us</div>
 
 		<!-- User registration or sign in button -->
 		<div class="button_for_user"></div>
@@ -43,6 +48,7 @@
 			</div>
 		</div>
 		
+		<!-- A result book -->
 		<div class="result_book_search_wrapper">
 			<div class="result_book_search">
 
@@ -50,7 +56,7 @@
 
 				<article class="result_book_search_text">
 					<strong class="result_book_title">Jaggarenath</strong>
-					<div class="result_book_author">by Karla Talbeck</div>
+					<div class="result_book_author">by Karla Talb..</div>
 					<div class="result_book_price_wrap">
 						<span class="result_book_price_text">Sell for -</span>
 						<storng class="result_book_price">£5.30</storng>
@@ -59,9 +65,35 @@
 
 				<div class="result_book_add_button"><span class="result_book_add_button_text">Add To Sell Basket</span></div>
 			</div>
+			
+			<div class="result_book_extra_options_buttons">
+				<span class="result_book_added_book_sell_button">Sell now?</span>
+				<span class="result_book_added_book_add_again_button">Add again+</span>
+			</div>
+		</div>
+		
+		<!--  -->
+		<div class="result_book_search_wrapper">
+			<div class="result_book_search">
 
-			<span class="result_book_added_book_button">Sell now?</span>
-			<span class="result_book_added_book_button">Add again+</span>
+				<img src="http://www.tangentbooks.co.uk/product_images/a/short_story_vol2__99344.jpg" class="result_book_thumbnail_image">
+
+				<article class="result_book_search_text">
+					<strong class="result_book_title">Bristol Sho...</strong>
+					<div class="result_book_author">by Guy Mcgee</div>
+					<div class="result_book_price_wrap">
+						<span class="result_book_price_text">Sell for -</span>
+						<storng class="result_book_price">£7.80</storng>
+					</div>
+				</article>
+
+				<div class="result_book_add_button"><span class="result_book_add_button_text">Add To Sell Basket</span></div>
+			</div>
+			
+			<div class="result_book_extra_options_buttons">
+				<span class="result_book_added_book_sell_button">Sell now?</span>
+				<span class="result_book_added_book_add_again_button">Add again+</span>
+			</div>
 		</div>
 
 	</section>
