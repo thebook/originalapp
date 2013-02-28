cbbe0685bf91eaaa697d49d93f4fbe1529a82da7
Cikica <aleksandar.winter@gmail.com>
2013-02-28 13:06:02 +0000

    Styles coming along

--- home.php
+++ home.php
@@ -4,12 +4,15 @@
 
 	<section class="header"></section>
 
-	<section class="bar">				
+	<section class="bar">
+
 		<!-- Logo  -->
-		<div class="logo_for_bar"><span class="with-icon-logo"></span></div>
+		<div class="logo_for_bar">
+			<span class="with-icon-logo"></span>
+		</div>
 
 		<!-- Progress bar  -->
-		<!-- <div class="progress_icon_for_bar">						
+		<!-- <div class="progress_icon_for_bar">
 			<span class="progress_icon_for_bar_text">Confirm</span>
 		</div> -->
 
@@ -19,14 +22,17 @@
 		<div class="navigation_text_for_bar">Us</div>
 
 		<!-- User registration or sign in button -->
-		<div class="button_for_user"></div>
+		<div class="button_for_user">
+			<span class="with-icon-user"></span>
+			<span class="with-icon-user-arrow"></span>
+		</div>
 
 		<!-- Search Input -->
 		<div class="input_for_bar">	
 			<div class="field_for_input">
 				<input type="text" class="input_block_for_search">
 			</div>
-			<div class="button_for_input"></div>
+			<div class="button_for_input"><span class="with-icon-search"></span></div>
 		</div>
 
 	</section>
@@ -35,25 +41,43 @@
 		
 		<!-- Price checker -->
 		<div class="price_selection">
+			
 			<div class="price_selection_quote">Quote</div>
+			
+			<div class="price_selection_selection">
+				<span class="price_selection_box">
+					<span class="with-icon-selection-tick"></span>
+				</span>
+				<span class="price_selection_text">Freepost</span>
+			</div>
+
+			<div class="price_selection_selection">
+				<span class="price_selection_box"></span>
+				<span class="price_selection_text">Recyclabus</span>
+			</div>
 		</div>
 		
 		<!-- Books description title -->
 		<div class="search_books_description_title">
-			<div class="search_books_description_title_text">Search Results For: 'Yo Motha'</div>
+			<!-- <div class="search_books_description_title_text">Search Results For: 'Yo Motha'</div> -->
 
 			<div class="sell_and_buy_basket">
-				<div id="buy_basket" class="basket_stats"><span class="sell_basket_text">Sell</span><span class="sell_basket_number">(0)</span></div>
-				<div id="sell_basket" class="basket_stats"><span class="buy_basket_text">Buy</span><span class="buy_basket_number">(0)</span> | </div>
+				<div id="sell_basket" class="basket_stats"><span class="buy_basket_text">Buy : </span><span class="buy_basket_number">0</span></div>
+				<div id="buy_basket" class="basket_stats"><span class="sell_basket_text">Sell : </span><span class="sell_basket_number">0</span> |</div>
 			</div>
 		</div>
 		
 		<!-- A result book -->
 		<div class="result_book_search_wrapper_left">
-			<div class="result_book_search">
-
+			<div class="result_book_search_added">
+				
+				<!-- Info Icon -->
+				<span class="with-icon-info-for-book"></span>
+				
+				<!-- Books thumbnail image -->
 				<img src="http://farm8.staticflickr.com/7278/8166869046_393e5eecf4.jpg" class="result_book_thumbnail_image">
-
+				
+				<!-- Article book search -->
 				<article class="result_book_search_text">
 					<strong class="result_book_title">Jaggarenath</strong>
 					<div class="result_book_author">by Karla Talb..</div>
@@ -62,10 +86,17 @@
 						<storng class="result_book_price">£5.30</storng>
 					</div>
 				</article>
+				
+				<!-- Add book button -->
+				<div class="result_book_add_button">
+					<span class="with-icon-added-to-sell-basket-tick">
+						Added To Basket
+					</span>
+				</div>
 
-				<div class="result_book_add_button"><span class="result_book_add_button_text">Add To Sell Basket</span></div>
 			</div>
 			
+			<!-- Extra buttons -->
 			<div class="result_book_extra_options_buttons">
 				<span class="result_book_added_book_sell_button">Sell now?</span>
 				<span class="result_book_added_book_add_again_button">Add again+</span>
@@ -76,10 +107,12 @@
 		<div class="result_book_search_wrapper">
 			<div class="result_book_search">
 
+				<span class="with-icon-info-for-book"></span>
+
 				<img src="http://www.tangentbooks.co.uk/product_images/a/short_story_vol2__99344.jpg" class="result_book_thumbnail_image">
 
 				<article class="result_book_search_text">
-					<strong class="result_book_title">Bristol Sho...</strong>
+					<strong class="result_book_title">Bristol Short Stories</strong>
 					<div class="result_book_author">by Guy Mcgee</div>
 					<div class="result_book_price_wrap">
 						<span class="result_book_price_text">Sell for -</span>
@@ -87,7 +120,11 @@
 					</div>
 				</article>
 
-				<div class="result_book_add_button"><span class="result_book_add_button_text">Add To Sell Basket</span></div>
+				<div class="result_book_add_button">
+					<span class="result_book_add_button_text">
+						Add To Sell Basket
+					</span>
+				</div>
 			</div>
 			
 			<div class="result_book_extra_options_buttons">
@@ -99,6 +136,8 @@
 		<!-- Last wrapper -->
 		<div class="result_book_search_wrapper_right">
 			<div class="result_book_search">
+
+				<span class="with-icon-info-for-book"></span>
 
 				<img src="http://1.bp.blogspot.com/_nXknRDZBs0E/STIHwAsUmwI/AAAAAAAACfA/hkB8fkCiGY0/s400/tongue.jpg" class="result_book_thumbnail_image">
 
