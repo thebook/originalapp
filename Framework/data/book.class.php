92811d395c6928f90b95450b5c61009f795103fa
Cikica <aleksandar.winter@gmail.com>
2013-04-16 17:03:14 +0100

    Changes to database stuff

--- Framework/data/book.class.php
+++ Framework/data/book.class.php
@@ -5,65 +5,72 @@
 */
 class book extends alpha
 {
-	
+	$post_book_table;
+	$recyclabus_one_table;
+
 	function __construct()
 	{	
-		$layout = array();
-		
+		$this->post_book_table      = 'post_book';
+		$this->recyclabus_one_table = 'recyclabus_one';
+
 		$table->check_if_table_exists_if_not_create_one(array(
-			'table_name'  => $this->account_table,
-			'primary_key' => 'id',
+			'table_name'  => $this->post_book_table,
+			'primary_key' => 'item_sku',
 			'fields'      => array(
-				array(
-					'column_name'    => 'id',
-					'data_type'      => 'INT',
-					'auto_increment' => true,
-					'unique'         => false				 									 				
+				array( 
+					'column_name'    => 'item_sku',
+					'auto_increment' => true
+					'data_type'      => 'INT'
+				),
+				array( 
+					'column_name'    => 'external_product_id',
+					'data_type'      => 'varchar(13)'
+				),
+				array( 
+					'column_name'    => 'external_product_id_type',
+					'data_type'      => 'varchar(13)'
+				),
+				array( 
+					'column_name'    => 'item_name',
+					'data_type'      => 'varchar(100)'
+				),
+				array( 
+					'column_name'    => 'item_name',
+					'data_type'      => 'varchar(100)'
+				),
+				array( 
+					'column_name'    => 'author',
+					'data_type'      => 'varchar(100)'
+				),
+				array( 
+					'column_name'    => 'binding',
+					'data_type'      => 'varchar(22)'
+				),
+				array( 
+					'column_name'    => 'publication_date',
+					'data_type'      => 'date'
+				),
+				array( 
+					'column_name'    => 'standard_price',
+					'data_type'      => 'varchar(11)'
+				),
+				array( 
+					'column_name'    => 'quantaty',
+					'data_type'      => 'varchar(4)'
 				),
 				array(
-					'column_name' => 'email',
-					'unique'      => true,				
-					'data_type'   => 'varchar(30)' 
+					'column_name'    => 'condition_type',
+					'data_type'      => 'varchar(1)'
 				),
 				array(
- 					'column_name' => 'first_name',
-					'data_type'   => 'varchar(22)' 
+					'column_name'    => 'product_description',
+					'data_type'      => 'text'	
 				),
 				array(
-					'column_name'=> 'second_name',
-					'data_type'  => 'varchar(22)' 
-				),
-				array(
-					'column_name' => 'password',									
-					'data_type'   => 'varchar(30)' 
-				),
-				array(
-					'column_name' => 'credit',
-					'data_type'   => 'int'
-				),
-				array(
-					'column_name' => 'recieve_newsletter',
-					'data_type'   => 'tinyint'
-				),
-				array(
-					'column_name' => 'university',			
-					'data_type'   => 'varchar(55)' 
-				),
-				array(
-					'column_name' => 'history',		
-					'data_type'   => 'blob',
-					'null'        => true
-				),
-				array(
-					'column_name' => 'price_promise',	
-					'data_type'   => 'blob',
-					'null'        => true
-				),
-				array(
-					'column_name' => 'unaccepted_book',
-					'data_type'   => 'blob',
-					'null'        => true
-				),
+					'column_name'    => 'main_image_url',
+					'data_type'      => 'tinytext'
+				)
+
 			)
 		));
 	}
