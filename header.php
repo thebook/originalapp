ad47c9bf67e03646349d18639c68121df958250d
Cikica <aleksandar.winter@gmail.com>
2013-05-02 15:45:55 +0100

    Recycla bus practitly done

--- header.php
+++ header.php
@@ -157,12 +157,12 @@
 				},
 				recyclabus : {
 					on : function () {
-						world.wrap.branch.recyclabus.self.css({ display : "block" }).animate({ opacity : 1 });
+						world.wrap.branch.bus.self.css({ display : "block" }).animate({ opacity : 1 });
 						animate.state = false;
 						animate.page  = "recyclabus";
 					},
 					off: function () { 
-						world.wrap.branch.recyclabus.self.css({ display : "none", opacity : 0 });
+						world.wrap.branch.bus.self.css({ display : "none", opacity : 0 });
 					}
 				},
 				sell: {
@@ -1667,7 +1667,7 @@
 							}
 						},
 						bus : {
-							self : '<section class="bus"></div>',
+							self : '<section class="bus pages"></div>',
 							branch : {
 								image : {
 									self : '<img src="'+frameworkuri+'/CSS/Includes/works/bus.png" class="bus_image">'
@@ -1908,7 +1908,12 @@
 									self : '<div class="bus_right_split"></div>',
 									branch : {
 										map : {
-											self : '<div class="bus_map"></div>'
+											self : '<div class="bus_map"></div>',
+											branch : {
+												map : {
+													self : '<iframe src="http://mapsengine.google.com/map/u/0/view?mid=zGeNIY0VmxqQ.kfujWjwn6WWo" width="390" height="390"></iframe>'
+												}
+											}
 										},
 										notify : {
 											self : '<div class="bus_signup"></div>',
@@ -1923,9 +1928,35 @@
 															self : '<div class="bus_signup_text">Leave you email and university and we\'ll email you when we\'re coming your way. Dont\'t worry wel\'ll only use this for reminders and nothing else</div>'
 														},
 														email : {
+															instructions : {
+																observe : {
+																	who      : state, 
+																	property : "signed",
+																	call     : function (change) {
+																		var input = world.wrap.branch.bus.branch.right_split.branch.notify.branch.body.branch.email.self;
+																		input.attr("readonly", change.new);
+																		input.val(state.account.email);
+																	}
+																}
+															},
 															self : '<input type="text" class="bus_signup_input" placeholder="Email">'
 														},
 														university : {
+															instructions : {
+																observe : {
+																	who      : state, 
+																	property : "signed",
+																	call     : function (change) {
+																		var input = world.wrap.branch.bus.branch.right_split.branch.notify.branch.body.branch.university.self;
+																		if ( change.new && state.account.university !== "" ) {
+																			input.attr("readonly", true);
+																			input.val(state.account.university);
+																		} else {
+																			input.attr("readonly", false);
+																		}
+																	}
+																}
+															},
 															self : '<input type="text" class="bus_signup_input" placeholder="University">'
 														},
 														button : {
@@ -1939,161 +1970,161 @@
 								}
 							}
 						},
-						recyclabus : {
-							self   : '<section class="recyclabus pages"></section>',
-							branch : {
-								left : {
-									self   : '<div class="recyclabus_half_left"></div>',
-									branch : {
-										title : {
-											self : '<div class="recyclabus_title">Recycla</div>',
-											last_branch : { 
-												highlight : '<span class="recyclabus_title_highlight">Bus</span>'
-											}
-										},
-										short_description : {
-											self : '<div class="recyclabus_title_description">The idea of Recyclabus is to make it as simple as possible for you to sell your textbooks.</div>'
-										},
-										description : {
-											self   : '<div class="recyclabus_description_box"></div>',
-											last_branch : {
-												paragraph_one   : '<div class="recyclabus_description_box_title">What is recyclabus?</div>',
-												paragraph_two   : '<div class="recyclabus_description_box_paragraph">Each Recyclabus will be manned by one of our book buyers, ready with a bar-code scanner to scan your books and pay your instantly.</div>',
-												paragraph_three : '<div class="recyclabus_description_box_paragraph">The bus will be coming to your university towards the end of your exams, we\'ll be accepting over 1 million titles, so please visit this page nearer the time so you don\'t miss the bus and don\'t miss getting some money for the books you no longer need.</div>',
-												paragraph_four  : '<div class="recyclabus_description_box_white_paragraph">On the day of the event we\'ll also have lots of exciting giveaways, so stay tuned to whats going on and what were giving away.</div>'
-											}
-										}
-									}
-								},
-								right : {
-									self   : '<div class="recyclabus_half"></div>', 
-									branch : {
-										wrap : {
-											self   : '<div class="recyclabus_half_dates_wrap"></div>',
-											branch : {
-												dates : {
-													self   : '<div class="recyclabus_dates_wrap"></div>',
-													branch : {
-														level_one  : {
-															self   : '<div class="recyclabus_dates_level_one"></div>',
-															branch : {
-																text : {
-																	self   : '<div class="recyclabus_dates_level_one_text_wrap"></div>',
-																	last_branch : {
-																		title_one : '<div class="recyclabus_dates_title_one">Dates</div>',
-																		title_two : '<div class="recyclabus_dates_title_two">Released On</div>'
-																	}
-																},
-																icon : {
-																	self   : '<div class="recyclabus_dates_icon"></div>',
-																	last_branch : {
-																		image : '<img src="'+ frameworkuri +'/CSS/Includes/works/calendar.png">'
-																	}
-																}
-															}
-														},
-														level_two : {
-															self   : '<div class="recyclabus_dates_level_two"></div>',
-															branch : {
-																title_three : {
-																	self   : '<div class="recyclabus_dates_title_three">1</div>',
-																	last_branch : {
-																		super_script : '<span class="recyclabus_dates_title_three_superscript">st.</span>'
-																	}
-																},
-																title_four : {
-																	self : '<div class="recyclabus_dates_title_four">April</div>'
-																}
-															}
-														}
-													}
-												},
-												dates_highlight : {
-														self : '<div class="recyclabus_dates_highlight">If you give us your email and university well send you a reminder when were coming your way</div>'
-												},
-												inputs : {
-													self   : '<div class="recyclabus_dates_input_wrap"></div>',
-													branch : {
-														email : {
-															instructions : {
-																observe : {
-																	who      : state, 
-																	property : "signed",
-																	call     : function (change) {
-																		var input = world.wrap.branch.recyclabus.branch.right.branch.wrap.branch.inputs.branch.email.self;
-																		input.attr("readonly", change.new);
-																		input.val(state.account.email);
-																	}
-																}
-															},
-															self : '<input type="text" class="recyclabus_dates_first_input" placeholder="Email">'
-														},
-														university : {
-															instructions : {
-																observe : {
-																	who      : state, 
-																	property : "signed",
-																	call     : function (change) {
-																		var input = world.wrap.branch.recyclabus.branch.right.branch.wrap.branch.inputs.branch.university.self;
-																		if ( change.new && state.account.university !== "" ) {
-																			input.attr("readonly", true);
-																			input.val(state.account.university);
-																		} else {
-																			input.attr("readonly", false);
-																		}
-																	}
-																}
-															},
-															self : '<input type="text" class="recyclabus_dates_seach_input" placeholder="University">'
-														},
-														text : {
-															self : '<div class="recyclabus_dates_input_text"><strong class="with-icon-lock-for-strong">Don\'t worry,</strong> well only use this information to remind you when you can sell your books</div>'
-														}
-													}
-												},
-												buttons_wrap : {
-													self   : '<div class="recyclabus_dates_button_wrap"></div>',
-													branch : {
-														inner_wrap : {
-															self   : '<div class="recyclabus_dates_inner_wrap"></div>',
-															last_branch : { 
-																first_button  : '<div class="recyclabus_dates_first_button">Notify me about recyclabus</div>',
-																second_button : '<div class="recyclabus_dates_second_button">Thank you for signing up</div>'
-															}
-														}
-													}
-												}
-											}
-										},
-										dates_decorational_arrow : {
-											self : '<div class="with-icon-dates-down-arrow"></div>',
-										},
-										extra_box : {
-											self   : '<div class="recyclabus_dates_extra_box"></div>',
-											branch : {
-												clock_icon : { 
-													self : '<div class="with-icon-dates-clock"></div>'
-												},
-												text : {
-													self   : '<div class="recyclabus_dates_extra_text_wrap"></div>',
-													branch : {
-														title : { 
-															self : '<div class="recyclabus_dates_extra_text_title">No Time To Lose?</div>'
-														},
-														text : {
-															self   : '<div class="recyclabus_dates_extra_text">Check out how to sell by freepost and sell to us now</div>',
-															last_branch : {
-																text_icon : '<div class="recyclabus_dates_extra_text_icon"><span data-function-instructions="{\'page\' : \'homepage_body_wrap\'}" data-function-to-call="front.prototype.change_page"  class="with-icon-recyclabus-dates-extra-text"></span></div>'
-															}
-														}
-													}
-												}
-											}
-										}
-									}
-								}
-							}
-						},
+						// recyclabus : {
+						// 	self   : '<section class="recyclabus pages"></section>',
+						// 	branch : {
+						// 		left : {
+						// 			self   : '<div class="recyclabus_half_left"></div>',
+						// 			branch : {
+						// 				title : {
+						// 					self : '<div class="recyclabus_title">Recycla</div>',
+						// 					last_branch : { 
+						// 						highlight : '<span class="recyclabus_title_highlight">Bus</span>'
+						// 					}
+						// 				},
+						// 				short_description : {
+						// 					self : '<div class="recyclabus_title_description">The idea of Recyclabus is to make it as simple as possible for you to sell your textbooks.</div>'
+						// 				},
+						// 				description : {
+						// 					self   : '<div class="recyclabus_description_box"></div>',
+						// 					last_branch : {
+						// 						paragraph_one   : '<div class="recyclabus_description_box_title">What is recyclabus?</div>',
+						// 						paragraph_two   : '<div class="recyclabus_description_box_paragraph">Each Recyclabus will be manned by one of our book buyers, ready with a bar-code scanner to scan your books and pay your instantly.</div>',
+						// 						paragraph_three : '<div class="recyclabus_description_box_paragraph">The bus will be coming to your university towards the end of your exams, we\'ll be accepting over 1 million titles, so please visit this page nearer the time so you don\'t miss the bus and don\'t miss getting some money for the books you no longer need.</div>',
+						// 						paragraph_four  : '<div class="recyclabus_description_box_white_paragraph">On the day of the event we\'ll also have lots of exciting giveaways, so stay tuned to whats going on and what were giving away.</div>'
+						// 					}
+						// 				}
+						// 			}
+						// 		},
+						// 		right : {
+						// 			self   : '<div class="recyclabus_half"></div>', 
+						// 			branch : {
+						// 				wrap : {
+						// 					self   : '<div class="recyclabus_half_dates_wrap"></div>',
+						// 					branch : {
+						// 						dates : {
+						// 							self   : '<div class="recyclabus_dates_wrap"></div>',
+						// 							branch : {
+						// 								level_one  : {
+						// 									self   : '<div class="recyclabus_dates_level_one"></div>',
+						// 									branch : {
+						// 										text : {
+						// 											self   : '<div class="recyclabus_dates_level_one_text_wrap"></div>',
+						// 											last_branch : {
+						// 												title_one : '<div class="recyclabus_dates_title_one">Dates</div>',
+						// 												title_two : '<div class="recyclabus_dates_title_two">Released On</div>'
+						// 											}
+						// 										},
+						// 										icon : {
+						// 											self   : '<div class="recyclabus_dates_icon"></div>',
+						// 											last_branch : {
+						// 												image : '<img src="'+ frameworkuri +'/CSS/Includes/works/calendar.png">'
+						// 											}
+						// 										}
+						// 									}
+						// 								},
+						// 								level_two : {
+						// 									self   : '<div class="recyclabus_dates_level_two"></div>',
+						// 									branch : {
+						// 										title_three : {
+						// 											self   : '<div class="recyclabus_dates_title_three">1</div>',
+						// 											last_branch : {
+						// 												super_script : '<span class="recyclabus_dates_title_three_superscript">st.</span>'
+						// 											}
+						// 										},
+						// 										title_four : {
+						// 											self : '<div class="recyclabus_dates_title_four">April</div>'
+						// 										}
+						// 									}
+						// 								}
+						// 							}
+						// 						},
+						// 						dates_highlight : {
+						// 								self : '<div class="recyclabus_dates_highlight">If you give us your email and university well send you a reminder when were coming your way</div>'
+						// 						},
+						// 						inputs : {
+						// 							self   : '<div class="recyclabus_dates_input_wrap"></div>',
+						// 							branch : {
+						// 								email : {
+						// 									instructions : {
+						// 										observe : {
+						// 											who      : state, 
+						// 											property : "signed",
+						// 											call     : function (change) {
+						// 												var input = world.wrap.branch.recyclabus.branch.right.branch.wrap.branch.inputs.branch.email.self;
+						// 												input.attr("readonly", change.new);
+						// 												input.val(state.account.email);
+						// 											}
+						// 										}
+						// 									},
+						// 									self : '<input type="text" class="recyclabus_dates_first_input" placeholder="Email">'
+						// 								},
+						// 								university : {
+						// 									instructions : {
+						// 										observe : {
+						// 											who      : state, 
+						// 											property : "signed",
+						// 											call     : function (change) {
+						// 												var input = world.wrap.branch.recyclabus.branch.right.branch.wrap.branch.inputs.branch.university.self;
+						// 												if ( change.new && state.account.university !== "" ) {
+						// 													input.attr("readonly", true);
+						// 													input.val(state.account.university);
+						// 												} else {
+						// 													input.attr("readonly", false);
+						// 												}
+						// 											}
+						// 										}
+						// 									},
+						// 									self : '<input type="text" class="recyclabus_dates_seach_input" placeholder="University">'
+						// 								},
+						// 								text : {
+						// 									self : '<div class="recyclabus_dates_input_text"><strong class="with-icon-lock-for-strong">Don\'t worry,</strong> well only use this information to remind you when you can sell your books</div>'
+						// 								}
+						// 							}
+						// 						},
+						// 						buttons_wrap : {
+						// 							self   : '<div class="recyclabus_dates_button_wrap"></div>',
+						// 							branch : {
+						// 								inner_wrap : {
+						// 									self   : '<div class="recyclabus_dates_inner_wrap"></div>',
+						// 									last_branch : { 
+						// 										first_button  : '<div class="recyclabus_dates_first_button">Notify me about recyclabus</div>',
+						// 										second_button : '<div class="recyclabus_dates_second_button">Thank you for signing up</div>'
+						// 									}
+						// 								}
+						// 							}
+						// 						}
+						// 					}
+						// 				},
+						// 				dates_decorational_arrow : {
+						// 					self : '<div class="with-icon-dates-down-arrow"></div>',
+						// 				},
+						// 				extra_box : {
+						// 					self   : '<div class="recyclabus_dates_extra_box"></div>',
+						// 					branch : {
+						// 						clock_icon : { 
+						// 							self : '<div class="with-icon-dates-clock"></div>'
+						// 						},
+						// 						text : {
+						// 							self   : '<div class="recyclabus_dates_extra_text_wrap"></div>',
+						// 							branch : {
+						// 								title : { 
+						// 									self : '<div class="recyclabus_dates_extra_text_title">No Time To Lose?</div>'
+						// 								},
+						// 								text : {
+						// 									self   : '<div class="recyclabus_dates_extra_text">Check out how to sell by freepost and sell to us now</div>',
+						// 									last_branch : {
+						// 										text_icon : '<div class="recyclabus_dates_extra_text_icon"><span data-function-instructions="{\'page\' : \'homepage_body_wrap\'}" data-function-to-call="front.prototype.change_page"  class="with-icon-recyclabus-dates-extra-text"></span></div>'
+						// 									}
+						// 								}
+						// 							}
+						// 						}
+						// 					}
+						// 				}
+						// 			}
+						// 		}
+						// 	}
+						// },
 						sell : {
 							self : '<section class="body pages"></section>',
 							branch : {
